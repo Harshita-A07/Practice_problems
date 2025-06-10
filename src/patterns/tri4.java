6de690bd8dc58e512024939e0f1d183b63755633
Harshita-A07 <78.papa.harshi@gmail.com>
2025-06-10 16:38:11 +0530

    new pattern

---
+++ src/patterns/tri4.java
@@ -0,0 +1,20 @@
+package patterns;
+
+public class tri4 {
+
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 0; i < rows; i++) {
+            // Print leading spaces
+            for (int s = 0; s < rows - i - 1; s++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            // Move to next line
+            System.out.println();
+        }
+    }
+}
