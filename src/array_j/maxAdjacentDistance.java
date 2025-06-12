76af4cc81e684a83e8d5bdcd0cc05fe0dda053eb
Harshita-A07 <78.papa.harshi@gmail.com>
2025-06-12 23:30:31 +0530

    max diff btwn ajacent elements in Circular array

---
+++ src/array_j/maxAdjacentDistance.java
@@ -0,0 +1,39 @@
+package array_j;
+
+import java.util.*;
+
+public class maxAdjacentDistance {
+
+    public static int maxAdjacentDistance(int[] nums) {
+        
+int size = nums.length;
+int start = 0;
+int maxdiff=0;
+
+        for (int i = 0; i < size; i++) {
+            int index = (i+1) % size;
+            int diff =Math.abs(nums[i] - nums[index]);
+            maxdiff = Math.max(maxdiff,diff);
+        }
+return maxdiff;
+    }
+
+
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = maxAdjacentDistance(nums);
+        System.out.println("Maximum adjacent distance: " + result);
+    }
+
+}
