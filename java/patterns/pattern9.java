75fd413e39db72b789ae06912d60c68afde4a3dc
Harshita-A07 <78.papa.harshi@gmail.com>
2025-06-17 11:23:15 +0530

    num pattern rigth tria

---
+++ java/patterns/pattern9.java
@@ -0,0 +1,15 @@
+package JAVA.patterns;
+
+public class pattern9 {
+    public static void main (String[] args){
+        int n=3;
+        for(int i=1;i<=n;i++){
+            int start=i;
+            for(int j=1;j<=i;j++){
+                System.out.print(start+" ");
+                start++;
+            }
+            System.out.println(" ");
+        }
+    }
+}
