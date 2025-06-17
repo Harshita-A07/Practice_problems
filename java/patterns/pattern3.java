d8b02f2a65c0474e5c8e8d495a1d01a7c63c1b23
Harshita-A07 <78.papa.harshi@gmail.com>
2025-06-17 11:08:38 +0530

    number pattern box

---
+++ java/patterns/pattern3.java
@@ -0,0 +1,22 @@
+package JAVA.patterns;
+
+// 9 8 7 
+// 6 5 4
+// 3 2 1
+
+public class pattern3 {
+    
+    public static void main (String[] args){
+        int n=3;
+        int start =n *n;
+        for(int i=0;i<n;i++){
+           for(int j=0;j<n;j++){
+        System.out.print(start--+" ");
+      
+    }
+     System.out.println(); 
+} 
+   
+  }
+}
+
