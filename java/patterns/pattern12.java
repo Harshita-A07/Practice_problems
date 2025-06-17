6d7033a8f254780198e2edf38991ed770416e342
Harshita-A07 <78.papa.harshi@gmail.com>
2025-06-17 11:30:11 +0530

    cone trian  pattern

---
+++ java/patterns/pattern12.java
@@ -0,0 +1,37 @@
+package JAVA.patterns;
+
+import java.util.Scanner;
+public class pattern12 {
+    public static void main(String[] args) {
+        
+    
+    for(int i=0;i<=5;i++)
+        {
+             for(int j=0;j<i;j++)
+      
+             {
+                System.out.print(" ");
+                
+            }
+            
+            for(int k=0;k<2*5 -(2*i+1);k++)
+      
+            {
+               System.out.print("*");
+               
+           }
+            
+           for(int l=0;l<=i;l++)
+      
+           {
+              System.out.print(" ");
+              
+          }
+           System.out.println();
+
+
+    }
+}
+    
+}
+
