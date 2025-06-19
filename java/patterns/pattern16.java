9d9e023b13cc87ba6e6ce77fb9b717ee9cf38526
Harshita-A07 <78.papa.harshi@gmail.com>
2025-06-19 10:55:22 +0530

    alphabet pattern16

---
+++ java/patterns/pattern16.java
@@ -0,0 +1,18 @@
+package JAVA.patterns;
+
+
+
+public class pattern16{
+    public static void main(String[] args){
+  int n=3;
+    for(int i=0;i<n;i++){
+        char ch =(char)('A'+i);
+        for(int j=0;j<=i;j++){
+            System.out.print(ch +" ");
+           
+        }
+        System.out.println( );
+    }
+
+}
+}
